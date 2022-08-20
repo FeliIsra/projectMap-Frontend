f78ebd45e67a9ce35a19d4ded661a907658735f6
Lazaro Gandolfo <lazarogandolfo@gmail.com>
2022-08-20 20:22:34 -0300

    fix: change loading value

--- src/redux/reducers/user.reducer.js
+++ src/redux/reducers/user.reducer.js
@@ -2,7 +2,7 @@ import * as constants from 'redux/contansts/user.constants';
 
 export const defaultState = {
   data: null,
-  loading: true,
+  loading: false,
 };
 
 const userReducer = (state = defaultState, action) => {
