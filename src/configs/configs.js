63ecb9508069ddab2c585e2585cc16e6a9559a4e
FeliIsra <fisrael@est.frba.utn.edu.ar>
2022-10-13 23:53:19 -0300

    change env var

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+export const API_BASE_URL = process.env.REACT_APP_BACKEND;
 
 export const ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT;
