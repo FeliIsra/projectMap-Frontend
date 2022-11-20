d3b0f8ec01338f6a38da10ea6bbde9a3c6d3f83f
Lazaro Gandolfo <lazarogandolfo@gmail.com>
2022-11-20 10:18:03 -0300

    fix: add calendly link validation (#11)

--- src/helpers/validateField.js
+++ src/helpers/validateField.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 export const validateField = (value) => {
   let error;
   if (!value) error = 'Campo requerido.';
@@ -9,4 +10,11 @@ export const validateFielWithNoZero = (value) => {
   let error;
   if (!value) error = 'Campo requerido.';
   return error;
+};
+
+export const validateCalendlyLink = (value) => {
+  const regex = new RegExp('https://calendly.com/[a-zA-Z0-9]*/[0-9]{2}min');
+  return regex.test(value)
+    ? undefined
+    : 'Link invalido. Formato valido: https://calendly.com/{nombreUsuario}/{tipoReunion}';
 };
